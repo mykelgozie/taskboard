0eaf4389b01437765dd9deb5ff83ec80f52072bd
michael <mykelgozie@gmail.com>
2021-02-14 00:02:35 +0100

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {DialogboxComponent} from "../app/dialogbox/dialogbox.component"
+import { faThumbtack } from '@fortawesome/free-solid-svg-icons'
+import { faTasks } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt} from '@fortawesome/free-solid-svg-icons'
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'board';
+  hidden = false;
+  pin = faThumbtack;
+  tasks = faTasks;
+  delete = faTrashAlt;
+  constructor(public dialog: MatDialog) {}
+
+  
+  openDialog() {
+    this.dialog.open(DialogboxComponent);
+  }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/angularComponent/angular.component';
+import { DialogboxComponent } from './dialogbox/dialogbox.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogboxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
